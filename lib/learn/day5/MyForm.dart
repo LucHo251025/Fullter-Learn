756f2e2bb507ac177269fae355bf0b219fa26557
Luc <lucht.23itb@vku.udn.vn>
2025-06-11 17:04:52 +0700

    finnis form basic

--- lib/learn/day5/MyForm.dart
+++ lib/learn/day5/MyForm.dart
@@ -6,6 +6,10 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  //Su dung GlobalKey de quan ly form
+  final _formKey = GlobalKey<FormState>();
+  String? _name;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,10 +17,47 @@ class _MyFormState extends State<MyForm> {
         title: Text("Form"),
       ),
       body: Padding(
-          padding: EdgeInsets.all(16.0),
-        // child: Form(
-        //   key: _formKey,
-        //     child: ),
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    hintText: "Your name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  onSaved: (value){
+                    _name = value;
+                  },
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        _formKey.currentState!.save();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Hello $_name"))
+                        );
+                      }
+                    }, child: Text("Submit")),
+                    ElevatedButton(onPressed: () {}, child: Text("Reset"))
+                  ],
+
+                )
+              ],
+            )
+        ),
       ),
     );
   }
