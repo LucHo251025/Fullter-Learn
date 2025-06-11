093a79ef77ea1ab8eda299901ad4edea6f742f9b
Luc <lucht.23itb@vku.udn.vn>
2025-06-11 21:42:49 +0700

    validate form

--- lib/learn/day5/MyForm.dart
+++ lib/learn/day5/MyForm.dart
@@ -8,7 +8,12 @@ class MyForm extends StatefulWidget {
 class _MyFormState extends State<MyForm> {
   //Su dung GlobalKey de quan ly form
   final _formKey = GlobalKey<FormState>();
+  final _fullNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
   String? _name;
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +28,7 @@ class _MyFormState extends State<MyForm> {
             child: Column(
               children: [
                 TextFormField(
+                  controller: _fullNameController,
                   decoration: InputDecoration(
                     labelText: "Name",
                     hintText: "Your name",
@@ -36,10 +42,76 @@ class _MyFormState extends State<MyForm> {
                   onSaved: (value){
                     _name = value;
                   },
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Please enter your name";
+                    }
+                    return null;
+                  },
 
                 ),
                 SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Your email",
+                    helperText: "Please enter a valid email address",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                    suffixIcon: Icon(Icons.clear),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Please enter your email";
+                    }
+                    if(!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').hasMatch(value)){
+                      return "Please enter a valid email address";
+                    }
+
+                    return null;
+
+                  },
+                ),
+                SizedBox(
                   height: 50,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: _obscurePassword,
+                  obscuringCharacter: "*",
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Your password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(onPressed: (){
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    }, icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off)),
+                  ),
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Please enter your password";
+                    }
+                    if(value.length < 6){
+                      return "Password must be at least 6 characters";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 30,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
