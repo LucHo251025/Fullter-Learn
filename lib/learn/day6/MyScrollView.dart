856efca92dedccef75fa1b009e972de5a9a7f43b
Luc <lucht.23itb@vku.udn.vn>
2025-06-12 14:46:19 +0700

    finish croll view

--- lib/learn/day6/MyScrollView.dart
+++ lib/learn/day6/MyScrollView.dart
@@ -21,198 +21,255 @@ class _FormCheckBoxState extends State<MyScrollView> {
       appBar: AppBar(
         title: Text("Form"),
       ),
-      body: Padding(
+      body:
+      Padding(
         padding: EdgeInsets.all(16.0),
         child: Form(
             key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _fullNameController,
-                  decoration: InputDecoration(
-                    labelText: "Name",
-                    hintText: "Your name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                      BorderSide(color: Colors.deepPurpleAccent, width: 2),
+            child: SingleChildScrollView(
+              child:  Column(
+                children: [
+                  TextFormField(
+                    controller: _fullNameController,
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      hintText: "Your name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
                     ),
-                    filled: true,
-                    fillColor: Colors.white,
+                    onSaved: (value) {
+                      _name = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
                   ),
-                  onSaved: (value) {
-                    _name = value;
-                  },
-                  validator: (value) {
+                  SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    controller: _fullNameController,
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      hintText: "Your name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    onSaved: (value) {
+                      _name = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    controller: _fullNameController,
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      hintText: "Your name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    onSaved: (value) {
+                      _name = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+
+
+                  TextFormField(
+
+                  ),
+
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text("Gioi tinh",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+
+                  ),
+                  FormField<String>(validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return "Please enter your name";
+                      return "Vui long chon gioi tinh";
                     }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-
-                TextFormField(
-
-                ),
-
-                SizedBox(
-                  height: 30,
-                ),
-                Text("Gioi tinh",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  }, builder: (FormFieldState<String> state) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                                child: RadioListTile(
+                                    title: Text("Fimale"),
+                                    value: "fimale",
+                                    groupValue: _gender,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _gender = value;
+                                        state.didChange(value);
+                                      });
+                                    })),
+                            Expanded(
+                                child: RadioListTile(
+                                    title: Text("Male"),
+                                    value: "male",
+                                    groupValue: _gender,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _gender = value;
+                                        state.didChange(value);
+                                      });
+                                    }))
+                          ],
+                        )
+                      ],
+                    );
+                  }),
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      hintText: "Your email",
+                      helperText: "Please enter a valid email address",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                      ),
+                      prefixIcon: Icon(Icons.email),
+                      suffixIcon: Icon(Icons.clear),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your email";
+                      }
+                      if (!RegExp(
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                          .hasMatch(value)) {
+                        return "Please enter a valid email address";
+                      }
 
-                ),
-                FormField<String>(validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Vui long chon gioi tinh";
-                  }
-                }, builder: (FormFieldState<String> state) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: _obscurePassword,
+                    obscuringCharacter: "*",
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Your password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          },
+                          icon: Icon(_obscurePassword
+                              ? Icons.visibility
+                              : Icons.visibility_off)),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      }
+                      if (value.length < 6) {
+                        return "Password must be at least 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  CheckboxListTile(title: Text("Dong y voi dieu khoang"),value:  _isAgreed, onChanged: (value){
+                    setState(() {
+                      _isAgreed = value!;
+                      print("Dong y: $_isAgreed");
+                    });
+                  },
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Row(
-                        children: [
-                          Expanded(
-                              child: RadioListTile(
-                                  title: Text("Fimale"),
-                                  value: "fimale",
-                                  groupValue: _gender,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _gender = value;
-                                      state.didChange(value);
-                                    });
-                                  })),
-                          Expanded(
-                              child: RadioListTile(
-                                  title: Text("Male"),
-                                  value: "male",
-                                  groupValue: _gender,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _gender = value;
-                                      state.didChange(value);
-                                    });
-                                  }))
-                        ],
-                      )
+                      ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text("Hello $_name")));
+                            }
+                          },
+                          child: Text("Submit")),
+                      ElevatedButton(
+                          onPressed: () {
+                            _formKey.currentState!.reset();
+                            setState(() {
+                              _name = null;
+                            });
+                          },
+                          child: Text("Reset"))
                     ],
-                  );
-                }),
-                SizedBox(
-                  height: 50,
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: "Your email",
-                    helperText: "Please enter a valid email address",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                      BorderSide(color: Colors.deepPurpleAccent, width: 2),
-                    ),
-                    prefixIcon: Icon(Icons.email),
-                    suffixIcon: Icon(Icons.clear),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your email";
-                    }
-                    if (!RegExp(
-                        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-                        .hasMatch(value)) {
-                      return "Please enter a valid email address";
-                    }
+                  )
+                ],
+              )),
+            )
 
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: _obscurePassword,
-                  obscuringCharacter: "*",
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    hintText: "Your password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                      BorderSide(color: Colors.deepPurpleAccent, width: 2),
-                    ),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        },
-                        icon: Icon(_obscurePassword
-                            ? Icons.visibility
-                            : Icons.visibility_off)),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your password";
-                    }
-                    if (value.length < 6) {
-                      return "Password must be at least 6 characters";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                CheckboxListTile(title: Text("Dong y voi dieu khoang"),value:  _isAgreed, onChanged: (value){
-                  setState(() {
-                    _isAgreed = value!;
-                    print("Dong y: $_isAgreed");
-                  });
-                },
-                  controlAffinity: ListTileControlAffinity.leading,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text("Hello $_name")));
-                          }
-                        },
-                        child: Text("Submit")),
-                    ElevatedButton(
-                        onPressed: () {
-                          _formKey.currentState!.reset();
-                          setState(() {
-                            _name = null;
-                          });
-                        },
-                        child: Text("Reset"))
-                  ],
-                )
-              ],
-            )),
       ),
     );
   }
