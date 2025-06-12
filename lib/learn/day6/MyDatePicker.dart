49739529c235edcf4b6806f5e108de40c7b966cf
Luc <lucht.23itb@vku.udn.vn>
2025-06-12 14:40:59 +0700

    finish datepicker

--- lib/learn/day6/MyDatePicker.dart
+++ lib/learn/day6/MyDatePicker.dart
@@ -7,6 +7,8 @@ class MyDatePicker extends StatefulWidget {
 }
 
 class _MyDatePickerState extends State<MyDatePicker> {
+  final _dateController = TextEditingController();
+  DateTime? _dateofBirth;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +16,43 @@ class _MyDatePickerState extends State<MyDatePicker> {
         title: const Text("My Date Picker"),
       ),
       body: Center(
-
+        child:
+        TextFormField(
+          validator:
+          (value) {
+            if (value == null || value.isEmpty) {
+              return "Please select your date of birth";
+            }
+            return null;
+          },
+          controller: _dateController,
+          decoration: InputDecoration(
+            labelText: "Date of Birth",
+            hintText: "Select your date of birth",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            suffixIcon: Icon(Icons.calendar_today)
+          ),
+          readOnly: true, //chi doc du lieu
+          onTap: () async {
+            DateTime? pickedDate = await showDatePicker(
+                context: context,
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2025));
+            if(pickedDate!=null) {
+              String formattedDate = DateFormat("dd/MM/yyyy").format(pickedDate);
+              setState(() {
+                _dateController.text = formattedDate;
+                _dateofBirth = pickedDate;
+              });
+            }
+          },
+        )
+        ,
       ),
     );
   }
