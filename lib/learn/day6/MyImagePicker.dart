824a11d3279ccbca6d770ea2aca5933909acdd6c
Luc <lucht.23itb@vku.udn.vn>
2025-06-12 22:03:52 +0700

    create MyImagePicker

---
+++ lib/learn/day6/MyImagePicker.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MyImagePicker extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyImagePickerState();
+}
+
+class _MyImagePickerState extends State<MyImagePicker> {
+  final _dateController = TextEditingController();
+  DateTime? _dateofBirth;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Date Picker"),
+      ),
+      body: Center(
+        child:
+        TextFormField(
+          validator:
+              (value) {
+            if (value == null || value.isEmpty) {
+              return "Please select your date of birth";
+            }
+            return null;
+          },
+          controller: _dateController,
+          decoration: InputDecoration(
+              labelText: "Date of Birth",
+              hintText: "Select your date of birth",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              suffixIcon: Icon(Icons.calendar_today)
+          ),
+          readOnly: true, //chi doc du lieu
+          onTap: () async {
+            DateTime? pickedDate = await showDatePicker(
+                context: context,
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2025));
+            if(pickedDate!=null) {
+              String formattedDate = DateFormat("dd/MM/yyyy").format(pickedDate);
+              setState(() {
+                _dateController.text = formattedDate;
+                _dateofBirth = pickedDate;
+              });
+            }
+          },
+        )
+        ,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/learn/day6/MyScrollView.dart';
+import 'package:flutter_learn/learn/day6/MyImagePicker.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyScrollView(),
+      home: MyImagePicker(),
     );
   }
 }
