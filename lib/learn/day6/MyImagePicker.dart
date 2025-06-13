b574c9795ce4f340255c0923f85ec9d00177aa66
Luc <lucht.23itb@vku.udn.vn>
2025-06-13 10:25:27 +0700

    create CRUP data

--- lib/learn/day6/MyImagePicker.dart
+++ lib/learn/day6/MyImagePicker.dart
@@ -1,59 +1,83 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-class MyImagePicker extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MyImagePickerState();
-}
-
-class _MyImagePickerState extends State<MyImagePicker> {
-  final _dateController = TextEditingController();
-  DateTime? _dateofBirth;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Date Picker"),
-      ),
-      body: Center(
-        child:
-        TextFormField(
-          validator:
-              (value) {
-            if (value == null || value.isEmpty) {
-              return "Please select your date of birth";
-            }
-            return null;
-          },
-          controller: _dateController,
-          decoration: InputDecoration(
-              labelText: "Date of Birth",
-              hintText: "Select your date of birth",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),
-              ),
-              filled: true,
-              fillColor: Colors.white,
-              suffixIcon: Icon(Icons.calendar_today)
-          ),
-          readOnly: true, //chi doc du lieu
-          onTap: () async {
-            DateTime? pickedDate = await showDatePicker(
-                context: context,
-                firstDate: DateTime(1900),
-                lastDate: DateTime(2025));
-            if(pickedDate!=null) {
-              String formattedDate = DateFormat("dd/MM/yyyy").format(pickedDate);
-              setState(() {
-                _dateController.text = formattedDate;
-                _dateofBirth = pickedDate;
-              });
-            }
-          },
-        )
-        ,
-      ),
-    );
-  }
-}
+// import 'dart:io';
+//
+// import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+// import 'package:image_picker/image_picker.dart';
+//
+// class MyImagePicker extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => _MyImagePickerState();
+// }
+//
+// class _MyImagePickerState extends State<MyImagePicker> {
+//   final _dateController = TextEditingController();
+//   final ImagePicker _picker = ImagePicker();
+//   File? _profileImage;
+//   DateTime? _dateofBirth;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("My Date Picker"),
+//       ),
+//       body:
+//       // FormField<File>(validator: (value) {
+//       //   if (value == null) {
+//       //     return "Vui long chon anh dai dien";
+//       //   }
+//       // }, builder: (FormFieldState<File> state) {
+//       //   return Column(
+//       //       crossAxisAlignment: CrossAxisAlignment.start,
+//       //       children: [
+//       //       GestureDetector(
+//       //       onTap: ()
+//       //   async{
+//       //     final XFile? image = await showDialog(
+//       //         context: context,
+//       //         builder: (context) =>
+//       //             AlertDialog(
+//       //               title: Text("Chon nguon"),
+//       //               content: Column(
+//       //                 mainAxisSize: MainAxisSize.min,
+//       //                 children: [
+//       //                   ListTile(
+//       //                     leading: Icon(Icons.photo_library),
+//       //                     title: Text("Thu vien anh"),
+//       //                     onTap: () async {
+//       //                       Navigator.pop(
+//       //                           context,
+//       //                           await _picker.pickImage(
+//       //                               source: ImageSource.gallery
+//       //                           )
+//       //                       );
+//       //                     },
+//       //                   ),
+//       //                   ListTile(
+//       //                     leading: Icon(Icons.camera_alt),
+//       //                     title: Text("Thu vien anh"),
+//       //                     onTap: () async {
+//       //                       Navigator.pop(
+//       //                           context,
+//       //                           await _picker.pickImage(
+//       //                               source: ImageSource.camera
+//       //                           )
+//       //                       );
+//       //                     },
+//       //                   )
+//       //                 ],
+//       //               ),
+//       //             )
+//       //     );
+//       //     if (image != null) {
+//       //       setState(() {
+//       //         _profileImage = File(image.path);
+//       //         state.didChange(_profileImage);
+//       //       });
+//       //     }
+//       //   }
+//       //   )
+//       // }),
+//     // );
+//   }
+// }
